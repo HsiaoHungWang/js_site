956c4f8dd47a815c6214a37ca07300e4c562b5a6
HsiaoHungWang <wang@ispan.com.tw>
2024-06-06 19:26:25 +0800

    PPT127 練習

--- scripts/site.js
+++ scripts/site.js
@@ -10,4 +10,69 @@ console.log(myDiv);  //null
 myDiv.innerHTML = "<h2>Hello World!!</h2>";
 */
 
-console.log("xyz\bxyz");
+//console.log("xyz\bxyz");
+
+//函式宣告式
+// function minNumber(num1, num2){
+//    let _min = num1;
+//    if(num1 > num2){
+//     _min = num2;
+//    }
+//    return _min;
+// }
+//函式表達式
+// const minNumber = function(num1, num2){
+//        let _min = num1;
+//    if(num1 > num2){
+//     _min = num2;
+//    }
+//    return _min;
+// }
+
+// //箭頭函式
+// const minNumber = (num1, num2) => {
+//     let _min = num1;
+// if(num1 > num2){
+//  _min = num2;
+// }
+// return _min;
+// }
+
+// //箭頭函式
+// const minNumber = (num1, num2) => {
+//     let _min = num1;
+// if(num1 > num2){
+//  _min = num2;
+// }
+// return _min;
+// }
+
+// //箭頭函式
+// const minNumber = (num1, num2) => {
+//     // ? :
+//   return  num1 > num2 ? num2 : num1;
+// }
+
+//箭頭函式
+const minNumber = (num1, num2) => num1 > num2 ? num2 : num1;
+
+
+const BCount = str => {
+  let count = 0;
+  for(let c of str){
+    if( c === "B"){
+      count++;
+    }
+  }
+  return count;
+}
+
+const charCount = (str, c) => {
+    let count = 0;
+    for(let _c of str){
+      if( _c === c){
+        count++;
+      }
+    }
+    return count;
+  }
